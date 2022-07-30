f2d7cffaaa5ba04abe4960c5c85eb0f078efc0c5
thewanionly <wani.pitogo@gmail.com>
2022-07-31 07:03:09 +0800

    Add empty cities list state

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -4,24 +4,33 @@ import { myCity } from '../utilities/constants'
 
 import StoreContext from '../store/storeContext'
 import CityCard from './CityCard'
+import { ReactComponent as EmptyCitiesImg } from '../assets/images/in_no_time.svg'
 
 const Cities = () => {
-  const { cities, handleShowModal } = useContext(StoreContext)
+  const { cities = [], handleShowModal } = useContext(StoreContext)
 
   return (
     <section className='cities'>
-      <div className='container cities__container'>
-        <div className='cities__city-list' data-testid='city-list'>
-          {cities.map((city) => (
-            <CityCard
-              key={city.timezone}
-              className='cities__city-item'
-              city={city}
-              baseCity={myCity}
-              handleShowModal={handleShowModal}
-            />
-          ))}
-        </div>
+      <div className='container'>
+        {cities.length > 0 ? (
+          <div className='cities__city-list' data-testid='city-list'>
+            {cities.map((city) => (
+              <CityCard
+                key={city.timezone}
+                className='cities__city-item'
+                city={city}
+                baseCity={myCity}
+                handleShowModal={handleShowModal}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className='cities__empty-image'>
+            <h4>No cities to show</h4>
+            <p>Click on "Add city" button above to add one.</p>
+            <EmptyCitiesImg />
+          </div>
+        )}
       </div>
     </section>
   )
