67b5afda3bccb2b759dfab29654bb9e28953631a
thewanionly <wani.pitogo@gmail.com>
2022-07-30 16:59:08 +0800

    Display "my-city" section. Shows my city's name (static) and my local clock in real-time

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from 'react'
+
+const useRealTimeClock = (timeZone) => {
+  const [date, setDate] = useState(new Date())
+
+  const localTime = date.toLocaleString('en-US', {
+    timeZone,
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false
+  })
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setDate(new Date()), 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  return [localTime]
+}
+
 const App = () => {
+  const myCity = 'Cebu City'
+  const myTImeZone = 'Asia/Manila'
+
+  const [localTime] = useRealTimeClock(myTImeZone)
+
   return (
-    <div>
-      <span>App</span>
+    <div className='app'>
+      <section className='my-city'>
+        <div className='container'>
+          <h3 data-testid='my-city-name'>{myCity}</h3>
+          <h1 data-testid='my-city-time'>{localTime}</h1>
+        </div>
+      </section>
     </div>
   )
 }
---
+++ src/App.test.jsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+
+const myCity = 'Cebu City'
+const myTImeZone = 'Asia/Manila'
+
+const setup = () => {
+  render(<App />)
+}
+
+const getLocalTime = (timeZone) => {
+  return new Date().toLocaleString('en-US', {
+    timeZone,
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false
+  })
+}
+
+describe('My City section', () => {
+  it('displays my city name', () => {
+    setup()
+
+    const myCityName = screen.getByTestId('my-city-name')
+    expect(myCityName.textContent).toBe(myCity)
+  })
+
+  it(`displays my local time in my city's timezone`, () => {
+    setup()
+
+    const myCityTime = screen.getByTestId('my-city-time')
+    expect(myCityTime.textContent).toBe(getLocalTime(myTImeZone))
+  })
+})
